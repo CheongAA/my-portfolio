81575ac1069fb357380ed721dc8713d2a56fd732
CheongAA <g92ki9@gmail.com>
2026-03-04 00:19:55 +0900

    feat: update home page with TechMarquee and CTAPanels

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,8 @@
 import { setRequestLocale } from "next-intl/server";
-import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/home/HeroSection";
 import AboutSection from "@/components/home/AboutSection";
-import ProjectsSection from "@/components/home/ProjectsSection";
-import ContactSection from "@/components/home/ContactSection";
+import TechMarquee from "@/components/home/TechMarquee";
+import CTAPanels from "@/components/home/CTAPanels";
 
 type Props = {
   params: Promise<{ locale: string }>;
@@ -14,15 +13,11 @@ export default async function HomePage({ params }: Props) {
   setRequestLocale(locale);
 
   return (
-    <div className="relative bg-background text-foreground overflow-hidden flex flex-col gap-50">
-      <header className="fixed w-full z-30 flex justify-center items-center pt-4 backdrop-blur">
-        <Navbar />
-      </header>
-
+    <>
       <HeroSection />
       <AboutSection />
-      <ProjectsSection />
-      <ContactSection />
-    </div>
+      <TechMarquee />
+      <CTAPanels />
+    </>
   );
 }
